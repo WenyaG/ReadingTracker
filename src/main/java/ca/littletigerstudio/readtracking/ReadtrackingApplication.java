b0eaed7d60f92d2953ca2ffe0dc2c3510e5ba5aa
WenyaG <guowenya0408@gmail.com>
2023-07-16 11:34:16 -0400

    add project files

---
+++ src/main/java/ca/littletigerstudio/readtracking/ReadtrackingApplication.java
@@ -0,0 +1,13 @@
+package ca.littletigerstudio.readtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadtrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadtrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/littletigerstudio/readtracking/ReadtrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.littletigerstudio.readtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadtrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
